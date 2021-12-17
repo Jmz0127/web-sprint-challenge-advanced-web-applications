267ef063b921316de14b6fd11814705932be7168
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-16 22:56:49 -0500

    added username and password input fields and appropriate ids

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Login = () => {
-    
-    return(<ComponentContainer>
-        <ModalContainer>
-            <h1>Welcome to Blogger Pro</h1>
-            <h2>Please enter your account information.</h2>
-        </ModalContainer>
-    </ComponentContainer>);
-}
+	return (
+		<ComponentContainer>
+			<ModalContainer>
+				<h1>Welcome to Blogger Pro</h1>
+				<h2>Please enter your account information.</h2>
+				<input id='username' placeholder='username' />
+				<input id='password' type='password' placeholder='password' />
+				<button>login</button>
+			</ModalContainer>
+		</ComponentContainer>
+	);
+};
 
 export default Login;
 
@@ -22,36 +26,36 @@ export default Login;
 //6. MAKE SURE TO ADD id="username", id="password", id="error" AND id="submit" TO THE APPROPRIATE DOM ELEMENTS. YOUR AUTOTESTS WILL FAIL WITHOUT THEM.
 
 const ComponentContainer = styled.div`
-    height: 70%;
-    justify-content: center;
-    align-items: center;
-    display:flex;
-`
+	height: 70%;
+	justify-content: center;
+	align-items: center;
+	display: flex;
+`;
 
 const ModalContainer = styled.div`
-    width: 500px;
-    background: white;
-    padding: 2rem;
-    text-align: center;
-`
+	width: 500px;
+	background: white;
+	padding: 2rem;
+	text-align: center;
+`;
 
 const Label = styled.label`
-    display: block;
-    text-align: left;
-    font-size: 1.5rem;
-`
+	display: block;
+	text-align: left;
+	font-size: 1.5rem;
+`;
 
 const FormGroup = styled.form`
-    padding:1rem;
-`
+	padding: 1rem;
+`;
 
 const Input = styled.input`
-    font-size: 1rem;
-    padding: 1rem 0;
-    width:100%;
-`
+	font-size: 1rem;
+	padding: 1rem 0;
+	width: 100%;
+`;
 
 const Button = styled.button`
-    padding:1rem;
-    width: 100%;
-`
+	padding: 1rem;
+	width: 100%;
+`;
