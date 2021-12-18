1fc830daf617f2ca6040ee77156d8eab6a2b7de5
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-17 19:52:17 -0500

    finished logout functionality, token is removed correctly

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
-    return(<div></div>);
-}
+const Logout = () => {
+	const { push } = useHistory();
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+
+		axiosWithAuth()
+			.post(
+				'/logout',
+				{},
+				{
+					headers: {
+						Authorization: token
+					}
+				}
+			)
+			.then((res) => {
+				localStorage.removeItem('token');
+				push('/login');
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	return <div></div>;
+};
 
 export default Logout;
 
